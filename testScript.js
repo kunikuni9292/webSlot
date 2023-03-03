12c27edcb539bdb38ffc68770f338e9176f5234a
くにくに <kunitatsu427@gmail.com>
2023-03-04 00:07:53 +0900

    不要なファイル削除

--- testScript.js
+++
@@ -1,7 +0,0 @@
-// Debug用に用意　のちに削除するもの
-// 起動時どうがを非表示にする
-document.getElementById("movie").style.display = "none";
-function test() {
-    var change = document.getElementById("movie");
-    change.style.display = change.style.display == "block" ? "none" : "block";
-}
