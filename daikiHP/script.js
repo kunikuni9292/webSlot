bee4de2e6df8921ad0a55fb54056b2ff383e3c1f
ダイキ <>
2023-04-29 08:32:36 +0900

    ハンバーガーメニューを画像に変更

--- daikiHP/script.js
+++ daikiHP/script.js
@@ -1,9 +1,9 @@
 $(".openbtn1").click(function() { //ボタンがクリックされたら
     $(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
+    $("#menu").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
 });
 
-$("#g-nav a").click(function() { //ナビゲーションのリンクがクリックされたら
+$("#menu a").click(function() { //ナビゲーションのリンクがクリックされたら
     $(".openbtn1").removeClass('active'); //ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
+    $("#menu").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
 });
