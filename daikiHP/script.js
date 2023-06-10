eb947b6b6d7a19f851604eebbd1db9a27d51a813
ダイキ <>
2023-06-10 18:01:19 +0900

    no message

--- daikiHP/script.js
+++ daikiHP/script.js
@@ -2,6 +2,11 @@ $(".openbtn1").mouseenter(function() { //ボタンがクリックされたら
     $(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
     $("#menu").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
 });
+// 画像orメニューからマウスが離れるとナビが閉じる
+$(".openbtn1, #menu").mouseleave(function() { // 画像またはメニューからマウスが離れたら
+    $(".openbtn1").removeClass('active'); // 画像の active クラスを除去
+    $("#menu").removeClass('panelactive'); // メニューの panelactive クラスを除去
+});
 
 $("#menu a").click(function() { //ナビゲーションのリンクがクリックされたら
     $(".openbtn1").removeClass('active'); //ボタンの activeクラスを除去し
