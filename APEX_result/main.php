93be50a1267c27a381da5d78ba9dd4556aedfdd8
くにくに <kunitatsu427@gmail.com>
2023-09-12 10:04:40 +0900

    20_APEX_result
    APEXの結果発表を表示するView
    アニメーション追加

---
+++ APEX_result/main.php
@@ -0,0 +1,468 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">ロゴだよー</div>
+        <div class="title">
+            <h1>▶試合結果◀<h1>
+        </div>
+        <div class="ab">
+            <p>match1-match3<br>■■■□□</p>
+        </div>
+    </div>
+    <div class="main">
+        <div class="left-box">
+            <div class="left-wrap">
+                <div class="index">
+                    <div class="rank-index">PLACE</div>
+                    <div class="team-index">TEAM</div>
+                    <div class="img-index"></div>
+                    <div class="kill-index">KILL</div>
+                    <div class="total-index">TOTAL</div>
+                </div>
+                <div class="big-box">
+                    <div class="rank-area">
+                        <div class="rank-box">
+                            <p class="abc">1</p>
+                        </div>
+                    </div>
+                    <div class="team-area">
+                        <p>チーム１</p>
+                    </div>
+                    <div class="img-area">
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                    </div>
+                    <div class="kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="big-box">
+                    <div class="rank-area">
+                        <div class="rank-box">
+                            <p class="abc">2</p>
+                        </div>
+                    </div>
+                    <div class="team-area">
+                        <p>チーム2</p>
+                    </div>
+                    <div class="img-area">
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                    </div>
+                    <div class="kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="big-box2">
+                    <div class="rank-area">
+                        <div class="rank-box">
+                            <p class="abc">3</p>
+                        </div>
+                    </div>
+                    <div class="team-area">
+                        <p>チーム3</p>
+                    </div>
+                    <div class="img-area">
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                        <div class="img-box"></div>
+                    </div>
+                    <div class="kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">4</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム4</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">5</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム5</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">6</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム6</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">7</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム7</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">8</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム8</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">9</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム9</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="middle-box"></div>
+        <div class="right-box">
+            <div class="right-wrap">
+                <div class="index">
+                    <div class="rank-index">PLACE</div>
+                    <div class="team-index">TEAM</div>
+                    <div class="img-index"></div>
+                    <div class="kill-index">KILL</div>
+                    <div class="total-index">TOTAL</div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">10</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム10</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">11</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム11</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">12</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム12</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">13</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム13</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">14</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム14</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">15</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム15</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">16</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム16</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">17</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム17</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">18</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム18</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">19</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム19</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+                <div class="mini-box">
+                    <div class="mini-rank-area">
+                        <div class="mini-rank-box">
+                            <p class="mini-abc">20</p>
+                        </div>
+                    </div>
+                    <div class="mini-team-area">
+                        <p>チーム20</p>
+                    </div>
+                    <div class="mini-img-area">
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                        <div class="mini-img-box"></div>
+                    </div>
+                    <div class="mini-kill-area">
+                        <p>5</p>
+                    </div>
+                    <div class="mini-total-area">
+                        <p>30</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
