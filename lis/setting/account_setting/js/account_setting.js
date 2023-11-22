896a9e81fe78371061fabf0198e683fb0c7e40da
くにくに <kunitatsu427@gmail.com>
2023-11-23 00:44:36 +0900

    firebaseの初期化を共通化　グローバル変数にてインスタンスを取得する方法にした。 歯車のアイコンのパスを修正 プロフィール画面の歯車のアイコンのパスを修正 アカウント情報をfireStoreに書き込む実装を追加 アカウント情報画面に遷移時、dbにアカウント情報があればテキストフィールドに反映させるように実装追加

--- lis/setting/account_setting/js/account_setting.js
+++ lis/setting/account_setting/js/account_setting.js
@@ -1,35 +1,81 @@
-// Firebaseを初期化
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyArCsB4IjxAyz50mPUU302pI3GRyfQerMY",
-  authDomain: "lis-web-app-116aa.firebaseapp.com",
-  projectId: "lis-web-app-116aa",
-  storageBucket: "lis-web-app-116aa.appspot.com",
-  messagingSenderId: "969669079165",
-  appId: "1:969669079165:web:84c42c31eba902de074114",
-  measurementId: "G-D9B3HTKLC6"
-});
 
-const auth = firebase.auth(firebaseApp);
-const provider = new firebase.auth.GoogleAuthProvider(firebaseApp);
+// ユーザー情報を取得し、存在する場合はデータを取得する処理
+const checkUserData = () => {
+  const auth = firebaseApp.auth();
+  const db = firebaseApp.firestore();
 
-// Googleログインメソッド
-const loginWithGoogle = () => {
-  // window.location.href = "../menu/menu_top/menu.html";
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const userSubCollection = db.collection("users").doc(user.uid).collection("user_data");
 
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      // Googleログイン成功
-      const user = result.user;
-      console.log(user);
-      window.location.href = "../menu/menu_top/menu.html";
-    })
-    .catch((error) => {
-      // エラー処理
-      alert(error.message);
-      console.error(error.code);
-      console.error(error.message);
-    });
+      // ユーザーデータがあるか確認
+      userSubCollection.get()
+        .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            // データが存在する場合、それをテキストフィールドに設定
+            const userData = querySnapshot.docs[0].data();
+            document.getElementById("personalSex").value = userData.sex || '';
+            document.getElementById("dateBirth").value = userData.dateBirth || '';
+            document.getElementById("height").value = userData.height || '';
+            document.getElementById("bodyWeight").value = userData.weight || '';
+          } else {
+            console.log("ユーザーデータはありません");
+          }
+        })
+        .catch((error) => {
+          console.error("Error getting user data:", error);
+        });
+    } else {
+      console.log("ユーザーはログインしていません");
+    }
+  });
 };
+
+// 画面読み込み時に実行
+window.onload = () => {
+  checkUserData(); // ユーザーデータの取得
+};
+
+// 登録関数内でユーザー情報を取得してサブコレクションを作成する
+const register = () => {
+  // firebaseの初期化インスタンス使用
+  const auth = window.auth;
+  const db = window.db;
+
+  // ユーザーがログインしていることを確認
+  const user = auth.currentUser;
+  if (user) {
+    // ユーザー情報を取得
+    const userEmail = user.email;
+    const userName = user.displayName;
+
+    // ここでフォームからのデータを取得
+    const sex = document.getElementById("personalSex").value;
+    const dateBirth = document.getElementById("dateBirth").value;
+    const height = document.getElementById("height").value;
+    const bodyWeight = document.getElementById("bodyWeight").value;
+
+    // ユーザーのUIDを使用してサブコレクションを作成
+    const userSubCollection = db.collection("users").doc(user.uid).collection("user_data");
+
+    // サブコレクションにデフォルトデータを追加
+    userSubCollection.add({
+      name: userName,
+      email: userEmail,
+      sex: sex,
+      dateBirth: dateBirth,
+      height: height,
+      weight: bodyWeight
+    }).then(() => {
+      console.log("Default data added to user's subcollection");
+    }).catch((error) => {
+      console.error("Error adding data to subcollection:", error);
+    });
+  } else {
+    console.error("ユーザーがログインしていません");
+  }
+};
+
 
 // 一つ前のページに戻る
 function goBack() {
