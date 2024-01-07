9f8bb7413f9639c243a22dd4da6fdb2edbd7d618
くにくに <kunitatsu427@gmail.com>
2024-01-08 01:02:56 +0900

    salon_ownerの権限カスタムクレームを追加 権限付与のこと

--- lis/functions/index.js
+++ lis/functions/index.js
@@ -2,7 +2,15 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: 'http://127.0.0.1:5500' });
 
-admin.initializeApp();
+// 初期化を1度だけ行うように変更
+if (!admin.apps.length) {
+    const serviceAccountKey = functions.config().serviceaccountkey;
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountKey),
+        databaseURL: 'https://lis-web-app-116aa.firebaseio.com'
+    });
+}
 
 exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
     const firebaseConfig = {
@@ -19,3 +27,17 @@ exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
         res.json(firebaseConfig);
     });
 });
+
+// サロンオーナーにオーナーの権限を付与
+const uid = 'oe3gJY5yn8ZMgK37ssTZjXbliCB3'; // くにのユーザーのUID
+const customClaims = { role: 'salon_owner' }; // カスタムクレーム
+
+admin.auth().setCustomUserClaims(uid, customClaims)
+    .then(() => {
+        console.log('カスタムクレームを設定しました');
+        // デプロイする前にFirebase Admin SDKが重複して初期化されないことを確認してください
+        return admin.app().delete();
+    })
+    .catch((error) => {
+        console.error('カスタムクレームの設定中にエラーが発生しました:', error);
+    });
